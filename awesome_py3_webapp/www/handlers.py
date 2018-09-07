629d80fa950037754e58c8915922d730b55f6a1f
github16cp <1229173308@qq.com>
2018-09-07 10:43:26 +0800

    180907

--- awesome_py3_webapp/www/handlers.py
+++ awesome_py3_webapp/www/handlers.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-__author__ = 'Michael Liao'
 
 ' url handlers '
 
@@ -19,8 +16,9 @@ async def index(request):
         'users': users
     }
 '''
+
 @get('/')
-def index(request):
+async def index(request):
     summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     blogs = [
         Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
@@ -31,3 +29,23 @@ def index(request):
         '__template__': 'blogs.html',
         'blogs': blogs
     }
+
+'''
+@get('/api/users')
+def api_get_users(*, page='1'):
+    page_index = get_page_index(page)
+    num = yield from User.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, users=())
+    users = yield from User.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    for u in users:
+        u.passwd = '******'
+    return dict(page=p, users=users)
+'''
+@get('/api/users')
+async def api_get_users():
+    users = await User.findAll(orderBy = 'created_at desc')
+    for u in users:
+        u.passwd = '******'
+    return dict(users = users)
