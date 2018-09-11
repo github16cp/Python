5a9a37d477b464473b2bfc494a22606d1a934ff3
github16cp <1229173308@qq.com>
2018-09-11 08:50:51 +0800

    180911

--- awesome_py3_webapp/www/handlers.py
+++ awesome_py3_webapp/www/handlers.py
@@ -9,7 +9,7 @@ import markdown2
 from aiohttp import web
 
 from coroweb import get, post
-from apis import APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
+from apis import Page, APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
 
 from models import User, Comment, Blog, next_id
 from config import configs
@@ -143,6 +143,13 @@ async def signout(request):
     logging.info('user signed out.')
     return r
 
+@get('/manage/blogs')
+def manage_blogs(*, page='1'):
+    return {
+        '__template__': 'manage_blogs.html',
+        'page_index': get_page_index(page)
+    }
+
 @get('/manage/blogs/create')
 async def manage_create_blog():
     return {
@@ -150,6 +157,16 @@ async def manage_create_blog():
         'id': '',
         'action': '/api/blogs'
     }
+
+@get('/api/blogs')
+async def api_blogs(*, page='1'):
+    page_index = get_page_index(page)
+    num = await Blog.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, blogs=())
+    blogs = await Blog.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    return dict(page=p, blogs=blogs)
 
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
@@ -194,3 +211,5 @@ async def api_create_blog(request, *, name, summary, content):
     blog = Blog(user_id=request.__user__.id, user_name=request.__user__.name, user_image=request.__user__.image, name=name.strip(), summary=summary.strip(), content=content.strip())
     await blog.save()
     return blog
+
+
